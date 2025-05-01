e857ecbe0a5a11f1fc80125ff42f8ba9729eb6f3
NovaAdamsDuma <137451209+NovaAdamsDuma@users.noreply.github.com>
2025-05-01 16:28:38 +0200

    Complete: DH and DH_Generate functions
    Completed the DH and DH_Generate functions. init function still incomplete. Will return there next

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,10 @@
 # Nova Adams-Duma: encryption implementation
+import nacl.bindings
+import nacl.public
+import os
+import time
+import hmac
+import hashlib
 
 class WireguardEncryption:
     # initialized encryption component with private / public keys
@@ -9,12 +15,19 @@ class WireguardEncryption:
         # Using eliptic curve cryptography, generate client's static public key
         self.client_static_public_key = nacl.bindings.crypto_scalarmult_base(self.client_static_private_key)
 
+    # function that generates new ephemeral key pair
     def DH_Generate(self):
+        private_key = nacl.public.PrivateKey.generate() # generate a PrivateKey object from the nacl library
+        return (private_key._private_key, private_key.public_key._public _key) # note: .public_key gets the amtching public key of the private key (from .private_key)
+        # return value is a tuple of both keys:
+        # Raw private key bytes: (private_key._private_key)
+        # Raw public key bytes: (private_key.public_key._public_key)
 
+    #function that performs Diffie-Helman key exchange with Curve25519 (see paper)
     def DH(self):
+        return nacl.bindings.crypto_scalarmult(private_key, public_key)
 
-    def DH_Generate(self):
-
+    # performs Authenticated Encryption with Associated Data (ChaCha20Poly1305)
     def AEAD_encrypt(self):
 
     def AEAD_decrypt(self):
