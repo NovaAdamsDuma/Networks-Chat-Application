ba949f4d244b93d559f8dc4697da8f6a919606ab
NovaAdamsDuma <137451209+NovaAdamsDuma@users.noreply.github.com>
2025-05-01 15:02:58 +0200

    Added function names
    Added expected functions for the encryption implementation

--- encrypt.py
+++ encrypt.py
@@ -5,5 +5,40 @@ class WireguardEncryption:
     def __init__(self, client_static_private_key, server_static_public_key):
         self.client_static_private_key = client_static_private_key # client's static private key
         self.server_static_public_key = server_static_public_key # server's static public key
+
+        # Using eliptic curve cryptography, generate client's static public key
+        self.client_static_public_key = nacl.bindings.crypto_scalarmult_base(self.client_static_private_key)
+
+    def DH_Generate(self):
+
+    def DH(self):
+
+    def DH_Generate(self):
+
+    def AEAD_encrypt(self):
+
+    def AEAD_decrypt(self):
+
+    def Hash(self):
+
+    def MixHash(self):
+
+    def Mac(self):
+
+    def Hmac(self):
     
-        
+    def Kdf1(self):
+
+    def Kdf2(self):
+    
+    def Kdf3(self):
+
+    def Timestamp(self):
+
+    def create_initiation_msg(self):
+    
+    def process_response_msg(self):
+    
+    def encrypt_msg(self):
+
+    def decrypt_msg(self):
