7fa985f0c7c02cf0d39f2ec9f9d298bf8c3e60a3
NovaAdamsDuma <137451209+NovaAdamsDuma@users.noreply.github.com>
2025-05-01 14:40:10 +0200

    Class creation and init function
    Created WireguardEncryption class and began coding "__init__" function

--- encrypt.py
+++ encrypt.py
@@ -1,1 +1,9 @@
 # Nova Adams-Duma: encryption implementation
+
+class WireguardEncryption:
+    # initialized encryption component with private / public keys
+    def __init__(self, client_static_private_key, server_static_public_key):
+        self.client_static_private_key = client_static_private_key # client's static private key
+        self.server_static_public_key = server_static_public_key # server's static public key
+    
+        
