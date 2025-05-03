5ca6fee7cefe1aba567057c68bd4dc14aa2e6bbd
NovaAdamsDuma <137451209+NovaAdamsDuma@users.noreply.github.com>
2025-05-04 01:57:12 +0200

    Methods for creating initiation message and handling response messages complete
    Will move on to testing now

--- encrypt.py
+++ encrypt.py
@@ -130,12 +130,119 @@ class WireguardEncryption:
 
     # function that creates handshake initiation message going to the server
     def create_initiation_msg(self):
+        self.sender_index = int.from_bytes(os.urandom(4), byteorder='little')
 
+        self.chain_key = self.Hash(self.CONSTRUCTION)
+        self.hash_value = self.Hash(self.chain_key + self.IDENTIFIER)
+        self.hash_value = self.Hash(self.hash_value + self.server_static_public)
+        
+        (self.ephemeral_private, self.ephemeral_public) = self.DH_Generate() # generating ephemeral key pair
+        
+        self.chain_key = self.Kdf1(self.chain_key, self.ephemeral_public) # Update chain key with ephemeral public key
+        self.hash_value = self.Hash(self.hash_value + self.ephemeral_public) # Update hash with ephemeral public key
+        
+        (self.chain_key, key1) = self.Kdf2(
+            self.chain_key, 
+            self.DH(self.ephemeral_private, self.server_static_public)
+        )
+        
+        msg_static = self.AEAD_encrypt( # encrypt static public key
+            key1, 0, self.client_static_public, self.hash_value
+        )
+        self.hash_value = self.Hash(self.hash_value + msg_static)
+        
+        (self.chain_key, key2) = self.Kdf2(
+            self.chain_key,
+            self.DH(self.client_static_private, self.server_static_public)
+        )
+    
+        timestamp = self.Timestamp()
+        msg_timestamp = self.AEAD_encrypt( # encrypt timestamp
+            key2, 0, timestamp, self.hash_value
+        )
+        self.hash_value = self.Hash(self.hash_value + msg_timestamp)
+        
+        # building the message
+        message = bytearray()
+        message.extend(b"\x01")  # Message type (1 = initiation)
+        message.extend(b"\x00\x00\x00")  # Reserved zeros
+        message.extend(self.sender_index.to_bytes(4, byteorder='little'))  # Sender index
+        message.extend(self.ephemeral_public)  # Unencrypted ephemeral public key
+        message.extend(msg_static)  # Encrypted static public key
+        message.extend(msg_timestamp)  # Encrypted timestamp
+        
+        # calculating MAC1
+        mac1_key = self.Hash(self.LABEL_MAC1 + self.server_static_public)
+        mac1 = self.Mac(mac1_key, message)
+        message.extend(mac1)
+        
+        # no implementation of mac2 given the requirements
+        message.extend(b"\x00" * 16)
+        
+        return bytes(message)
     
     # function that processes server's response to the handshake message
-    def process_response_msg(self):
+    def process_response_msg(self, responseMsg):
+        if len(responseMsg) < 56:
+            return False
+            
+        msg_type = responseMsg[0]
+        if msg_type != 0x02: # message parsing
+            return False
+            
+        sender_index = int.from_bytes(responseMsg[4:8], byteorder='little')
+        receiver_index = int.from_bytes(responseMsg[8:12], byteorder='little')
+        if receiver_index != self.sender_index:
+            return False
+            
+        self.receiver_index = sender_index
+        server_ephemeral_public = responseMsg[12:44]
+        msg_empty = responseMsg[44:60]  # 16 bytes for AEAD_LEN(0)
         
-    
+        mac1_key = self.Hash(self.LABEL_MAC1 + self.client_static_public) # verify Mac1
+        mac1 = self.Mac(mac1_key, responseMsg[:60])
+        if mac1 != responseMsg[60:76]:
+            return False
+            
+        
+        self.hash_value = self.Hash(self.hash_value + server_ephemeral_public) # update hash value with server's ephemeral public key
+        
+        self.chain_key = self.Kdf1(self.chain_key, server_ephemeral_public) # update chain key with server's ephemeral public key
+        
+        self.chain_key = self.Kdf1( # update chain key with DH result
+            self.chain_key,
+            self.DH(self.ephemeral_private, server_ephemeral_public)
+        )
+        
+        self.chain_key = self.Kdf1( # update chain key with DH result
+            self.chain_key,
+            self.DH(self.client_static_private, server_ephemeral_public)
+        )
+        
+        (self.chain_key, temp, key3) = self.Kdf3(self.chain_key, b"") # deriving final transport keys
+        self.hash_value = self.Hash(self.hash_value + temp)
+        
+        try: # decrypting the empty message for verifications
+            empty = self.AEAD_decrypt(key3, 0, msg_empty, self.hash_value)
+            if empty != b"":
+                return False
+        except:
+            return False
+            
+        self.hash_value = self.Hash(self.hash_value + msg_empty)
+        
+        (send_key, recv_key) = self.Kdf2(self.chain_key, b"") 
+        self.sending_key = send_key # derive final sending key
+        self.receiving_key = recv_key # derive final receiving key
+        self.sending_counter = 0
+        self.receiving_counter = 0
+        
+        self.chain_key = None # sensitive data, so must be cleared
+        self.hash_value = None # sensitive data, so must be cleared
+        self.ephemeral_private = None # sensitive data, so must be cleared
+        
+        return True
+        
     # function that encrypts a message to be sent to the server
     def encrypt_msg(self, plain_text):
         if not self.sending_key:
